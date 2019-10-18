af7194a9bc4321171090b55d784b430481ea72bb
Larry Fielding <larryjaguey@gmail.com>
2019-10-18 00:14:44 -0500

    se arregla la lógica del reenvio

--- servidor.cpp
+++ servidor.cpp
@@ -13,8 +13,8 @@ int main(int argc, char const *argv[])
     cout << "Soy servidor (cajero automático)" << endl;
 
     struct mensaje * msj;
-    char message[20];
-    int a = 0, b = 0, c = 0, nbd = 0, idRecibido = 0, idEsperado = 0, tmp = 0;
+    char message[20] = {0x0};
+    int a = 0, b = 0, c = 0, nbd = 0, idRecibido = 0, idEsperado = 1;
 
 	Respuesta respuesta = Respuesta(7200);
 
@@ -30,40 +30,40 @@ int main(int argc, char const *argv[])
             // Obtiene números de la cadena:
             sscanf(msj->arguments, "%d %d", &a, &b);
      		sprintf(message, "%d + %d = %d", a, b, a+b);
+            memset(message, 0, sizeof(message));
      		break;
         case deposito:
-            // Obtiene números de la cadena:
+        //TODO: caMBIar la logica, tambien responder si no llega:
             idRecibido = msj->requestId;
             if (idRecibido == 1)
             {
-                idEsperado = 0;
                 nbd = 0;
+                idEsperado = 1;
             }
-            sscanf(msj->arguments, "%d", &c);
-            tmp = nbd;
-            nbd += c;
-            sprintf(message, "%d", nbd);
+            if (idEsperado == idRecibido)
+            {
+                cout << "Saldo actual: $" << nbd << ".00\t";
+                sscanf(msj->arguments, "%d", &c);
+                nbd += c;
+                cout << "Nuevo saldo: $" << nbd << ".00" << endl;
+                sprintf(message, "%d", nbd);
+                respuesta.sendReply(message);
+                idEsperado ++;
+            }
+            else if (idEsperado == idRecibido + 1)
+            {
+                cout << "Saldo actual: $" << nbd << ".00" << ". Se reenvía, no se recalcula."<< endl;
+                sprintf(message, "%d", nbd);
+                respuesta.sendReply(message);
+            }
+            memset(message, 0, sizeof(message));
             c = 0;
             break;
  		default:
  			strcpy(message, "operación no soportada\n");
      		break;
      }
-
-     // Condición inicial
-     if (idEsperado == 0)
-     {
-         idEsperado = 1;
-     }
-
-     if (idEsperado == idRecibido)
-     {
-        respuesta.sendReply(message);
-        idEsperado = idRecibido + 1;
-        cout << "Saldo actual: $" << tmp << ".00" << endl;
-        cout << "Nuevo saldo: $" << nbd << ".00" << endl;
-     }
+     
   }
-    
 	return 0;
 }
